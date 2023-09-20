fd7efadc09edd21fdea34d52629084b272e8eefb
cheefung97 <shuaigeomg@gmail.com>
2023-09-20 20:07:14 +0800

    initial commit

---
+++ src/components/Footer.svelte
@@ -0,0 +1,14 @@
+<footer 
+class="py-20 sm:py-32 bg-black border-t border-solid border-violet-950
+flex flex-col gap-4 sm:gap-8 justify-center items-center">
+
+<p class='px-4 py-2 bg-white text-slate-950 font-medium'>
+    Connect with me &darr;
+</p>
+<div class="flex flex-col gap-4 items-center justify-center">
+    <p><b class="pr-2">Email:</b>ch33fung@gmail.com</p>
+    <p><b class="pr-2">Github:</b><a class="text-violet-400" target='_blank' href="https://github.com/cheefung33">cheefung33</a></p>
+    <p><b class="pr-2">LinkedIn:</b><a class="text-violet-400" target='_blank' href="https://linkedin.com/in/cheefung33">cheefung</a></p>
+    
+</div>
+</footer>
