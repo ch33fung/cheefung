61b1187a8bd8562a8a8978f9ae6ad49a81537714
cheefung <ch33fung@gmail.com>
2023-12-19 21:57:57 +0800

    ror

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -11,18 +11,18 @@
         <span class="poppins text-violet-400">Developer</span> 
       </h2>
       <p class='text-base sm:text-lg md:text-xl'>
-        My <span class="text-violet-400">favourite tech</span> includes Javascript (Node.JS
-        or SvelteKit), <br/>TailwindCSS, Express.JS & PostgreSQL!
+        My <span class="text-violet-400">favourite tech</span> includes Ruby on rails, Javascript (Node.JS
+        or SvelteKit), <br/>Bootstrap, TailwindCSS, Express.JS & PostgreSQL!
       </p>
       <a class="blueShadow mx-auto lg:mr-auto lg:ml-0 text-base sm:text-lg md:text-xl poppins
       relative overflow-hidden px-6 py-3 group rounded-full bg-white text-slate-950
       cursor-pointer" 
-      href="#"
+      href="https://wa.me/60102226151"
       target='_blank'>
 
       <div class='absolute top-0 right-full w-full h-full bg-violet-400 opacity-20
       group-hover:translate-x-full z-0 duration-200'/>
-      <h4 class="relative z-9"><a target='_blank' href="https:wa.me/60102226151">Get in Touch &rarr;</a></h4>
+      <h4 class="relative z-9">Get in touch &rarr;</h4>
       </a>
     </div>
     <div class='relative shadow-2xl grid place-items-center'>
@@ -33,11 +33,26 @@
     <section id='projects' class='py-20 lg:py-32 flex flex-col gap-24'>
         <div class='flex flex-col gap-2 text-center'>
             <h6 class='text-lg sm:text-xl md:text-2xl'>
-                Some of my endeavors.
+                Projects
             </h6>
-            <h3>
-                Curious
-            </h3>
+
+            <div id="showcase-container" class='row'>
+                <div class="col-sm-4 mb-5">
+                    <a href="images/projects/shapee.png">
+                        <img src="images/projects/shapee.png" class="img-fluid">
+                    </a>
+                </div>
+                <div class="col-sm-4 mb-5">
+                    <a href="images/projects/snipcart.png">
+                        <img src="images/projects/snipcart.png" class="img-fluid">
+                    </a>
+                </div> 
+                <div class="col-sm-4 mb-5">
+                    <a href="images/projects/ttt.png">
+                        <img src="images/projects/ttt.png" class="img-fluid">
+                    </a>
+                </div>
+            </div>
         </div>  
     </section>
     <section 
@@ -49,10 +64,10 @@
     before:left-0 before:w-2/3 before:h-1.5 before:bg-violet-700 after:absolute
     after:bottom-0 after:right-0 after:w-2/3 after:h-1.5 after:bg-violet-700 py-4'/>
 
-    <h6 class="text-lg sm:text-xl md:text-2xl">Know more about me</h6>
-    <h3 class="font-semibold text-3xl sm:text-4xl md:text-5xl">
-        A bit <span class="poppins text-violet-400">about</span> me.
-    </h3>
+    <!-- <h6 class="text-lg sm:text-xl md:text-2xl">Know more about me</h6> -->
+    <!-- <h3 class="font-semibold text-3xl sm:text-4xl md:text-5xl">
+        A passionate full-stack developer with a love for crafting web applications from end to end. On the frontend, I specialize in creating engaging user experiences using HTML, CSS, and JavaScript. I'm well-versed in modern frontend frameworks like React, Svelte, Next.js, and Express.js, and I enjoy optimizing web applications for performance and responsiveness. When it comes to the backend, I'm experienced in building server-side applications. I've worked with technologies such as Node.js and Ruby on rails ensuring that data is efficiently processed, stored, and served. Let's collaborate and turn your ideas into a reality! Feel free to reach out, and together, we can build innovative, user-centric web applications that make a <span class="poppins text-violet-400">difference</span>
+    </h3> -->
 
 
     </section>
