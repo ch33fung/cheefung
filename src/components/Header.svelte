62b80fd7e31f73ec5df224ef60562cd728f81e28
cheefung <ch33fung@gmail.com>
2023-12-19 22:14:16 +0800

    footer

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -3,6 +3,7 @@
     let tabs = [
         {name: 'Projects',link: '#projects'},
         // {name: 'About me',link: '#About'},
+        {name: 'Contact me',link: '#contactme'},
     ];
 </script>
 
@@ -19,14 +20,15 @@
           </a>
         {/each}  
     </div>
-    <a 
+    <!-- <a 
       href='#'
       target='_blank'
       class="blueShadow relative overflow-hidden px-5 py-2 group rounded-full bg-white
-    text-slate-950">
-      <div class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20
+    text-slate-950"> -->
+      <!-- <div class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20
       group-hover: translate-x-full z-0 duration-200">
       </div>
-      <h4 class="relative z-9"><a target='_blank' href="https:wa.me/60102226151">Get in Touch</h4>
-    </a>
+      <h4 class="relative z-9"><a target='_blank' href="https:wa.me/60102226151">Get in Touch</h4> -->
+        <!-- <h4 class="relative z-9"><a target='_blank' href="contactme">Get in Touch</h4> -->
+    <!-- </a> -->
 </header>
--- src/routes/contactme/+page.svelte
+++ src/routes/contactme/+page.svelte
@@ -1,15 +1,15 @@
-<footer 
+<header id='contact me' 
 class="py-20 sm:py-32 bg-black border-t border-solid border-violet-950
-flex flex-col gap-4 sm:gap-8 justify-center items-center"
-id='contactme'>
+flex flex-col gap-4 sm:gap-8 justify-center items-center">
 
 <p class='px-4 py-2 bg-white text-slate-950 font-medium'>
     Connect with me &darr;
 </p>
-<div class="flex flex-col gap-4 items-center justify-center">
+<div id= 'Get in touch' class="flex flex-col gap-4 items-center justify-center">
     <p><b class="pr-2">Email:</b><a class="text-violet-400" href="mailto:ch33fung@gmail.com">ch33fung@gmail.com</a></p>
     <p><b class="pr-2">Github:</b><a class="text-violet-400" target='_blank' href="https://github.com/ch33fung">ch33fung</a></p>
     <p><b class="pr-2">LinkedIn:</b><a class="text-violet-400" target='_blank' href="https://linkedin.com/in/ch33fung">ch33fung</a></p>
     <p><b class="pr-2">Whatsapp:</b><a class="text-violet-400" target='_blank' href="https://wa.me/60102226151">+60102226151</a></p>
 </div>
-</footer>
+</header>
+
